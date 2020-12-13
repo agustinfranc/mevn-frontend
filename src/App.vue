5996ce200a3ebef239ca882b104cae872502d771
Agustin Amato <agustin.amato96@gmail.com>
2020-12-13 16:15:21 -0300

    new navbar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login" v-if="!isAuthenticated">Login | </router-link>
-      <a href="#" @click="logout" v-if="isAuthenticated">Logout | </a>
-      <router-link to="/about">About</router-link> |
-      <router-link to="/notes" v-if="isAuthenticated">Notes</router-link>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">My Notes</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+            <b-nav-item to="/notes" v-if="isAuthenticated">Notes</b-nav-item>
+            <b-nav-item to="/notes" v-if="!isAuthenticated">Login</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <!-- <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search"
+              ></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search</b-button
+              >
+            </b-nav-form> -->
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>{{ user.name }}</em>
+              </template>
+              <b-dropdown-item href="#" @click="logout" v-if="isAuthenticated">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <router-view />
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
+import { mapState, mapActions, mapGetters } from "vuex";
 
 export default {
   computed: {
+    ...mapState(["user"]),
     ...mapGetters(["isAuthenticated"])
   },
   methods: {
