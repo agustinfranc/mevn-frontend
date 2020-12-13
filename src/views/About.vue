a5950fe643b52e1cb665b08929176033f2fbac6c
Agustin Amato <agustin.amato96@gmail.com>
2020-12-13 18:13:49 -0300

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container pt-3">
+    <img alt="Express logo" src="../assets/express.png" />
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="my-5">
+      <h5>
+        This is a small responsive MEVN (MongoDB + Express.js + Vue.js +
+        Node.js) project.
+      </h5>
+
+      <hr class="my-4" />
+
+      <p># Stateless Authentication with JWT</p>
+      <p># Implemented Vue Router and Vuex</p>
+      <p># Deployed on Heroku</p>
+      <p># With BootstrapVue framework</p>
+      <p># Linting with ESlint</p>
+
+      <b-link href="https://github.com/agustinfranc" target="_blank"><img alt="Express logo" src="../assets/github.jpg" /></b-link> | 
+      <b-link href="https://www.linkedin.com/in/agustin-amato-449006143/" target="_blank"><img alt="Linkedin logo" src="../assets/linkedin.jpg" /></b-link>
+    </div>
   </div>
 </template>
